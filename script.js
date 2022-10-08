4414183f9418d3d9eccb85d9f5e706d1087e1c2d
Abdurrahman_X <abolajiabdurrahman@gmail.com>
2022-10-08 19:59:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const restaurant = {
+    name: "Classico Italiano",
+    location: "Via Angelo Tavanti 23, Firenze, Italy",
+    categories: ['Italian', 'Pizerria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+}
+
+// ARRAY DESTRUCTURING
+
+/* 
+Destructuring is to break down a complex into a smaller data structure like a variable. For arrays, we use destructuring to retrieve elements from the array and store them to variables in a very easy way.
+*/
+
