95ef955ebc03c0639b9036576fe5ba9874f5c7f9
Abdurrahman_X <abolajiabdurrahman@gmail.com>
2022-10-10 11:24:57 +0100

    destructuring objects

--- script.js
+++ script.js
@@ -6,12 +6,82 @@ const restaurant = {
     categories: ['Italian', 'Pizerria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours:{
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // open 24 hours
+            close: 24,
+        },
+    },
 
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    }
+    },
+
+    orderDelivery: function ({ starterIndex = 0, mainIndex = 1, address, time = "20:00" }) {
+        //console.log(address, mainIndex, starterIndex, time);
+        console.log(`Order Received!, ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
 }
 
+restaurant.orderDelivery({
+    time: "23:30",
+    address: "4449 Bingamon Branch Road",
+    starterIndex: 2,
+    mainIndex: 2,
+});
+
+console.log(restaurant.order(1,2));
+
+console.log(restaurant.openingHours.thu);
+
+// OBJECT DESTRUCTURING
+// const { name, openingHours, categories } = restaurant; // Object destructuring does not care about the order like in array destructuring.
+// console.log(name, openingHours, categories);
+
+// To give new variables names
+const { 
+    name: restaurantName, 
+    openingHours: hours, 
+    categories: tags,
+ } = restaurant;
+
+ console.log(restaurantName, hours, tags);
+ 
+ // Setting Default Values -  Especialyy when we do not have data hardcoded as above.
+
+ const { menu = [], starterMenu: starters = []} = restaurant;
+ console.log(menu, starters);
+
+ // MUTATING VARIABLES
+ let a = 111;
+ let b = 999;
+ const obj = { a: 23, b: 7, c: 14};
+
+ ({ a, b } = obj);
+ console.log(a, b);
+
+ console.log(hours);
+
+ // Nested Objects
+
+ const { fri: {open, close}} = hours;
+ console.log(open, close);
+
+ 
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
 // ARRAY DESTRUCTURING
 
 /* 
@@ -19,9 +89,9 @@ Destructuring is to break down a complex into a smaller data structure like a va
 */
 
 const arr = [3, 4, 5]
-const a = arr[0]
-const b = arr[1]
-const c = arr[2]
+// const a = arr[0]
+// const b = arr[1]
+// const c = arr[2]
 
 // Destructuring example
 const [x, y, z] = arr;
@@ -44,11 +114,11 @@ let [main, , secondary] = restaurant.categories;
 
 // To switch values of the variables using destructuring
 [main, secondary] = [secondary, main]
-console.log(main, secondary);
+//console.log(main, secondary);
 
 // Receive two return values from a function.
 const [starter, mainCourse] = restaurant.order(1, 0);
-console.log(starter, mainCourse);
+//console.log(starter, mainCourse);
 
 // NESTED ARRAYS
 const nested = [2, 4, [5, 6]];
@@ -56,7 +126,7 @@ const nested = [2, 4, [5, 6]];
 //console.log(i, k); // 2, [5, 6]
 
 const [i, , [j, k]] = nested;
-console.log(i,j,k); // 2,5,6
+//console.log(i,j,k); // 2,5,6
 
 // DEFAULT VALUES
 // const [p, q, r] = [8, 9]
@@ -64,4 +134,4 @@ console.log(i,j,k); // 2,5,6
 
 //The above error can be fixed by setting default values as shown below:
 const [p = 1, q = 1, r = 1] = [8, 9]
-console.log(p, q, r); // 8, 9, 1 
+//console.log(p, q, r); // 8, 9, 1 
